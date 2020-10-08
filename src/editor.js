5d4b7eaf3e90813699dc4fbbdb6abbe8803f4757
Michelle Tanoto <michelletanoto@Michelles-MacBook-Pro-4.local>
2020-10-09 09:54:45 +1100

    added remove format

--- src/editor.js
+++ src/editor.js
@@ -1,3 +1,5 @@
+var textVersion = require("textversionjs");
+
 function createHTML(options = {}) {
     const {
         backgroundColor = '#FFF',
@@ -110,6 +112,10 @@ function createHTML(options = {}) {
             heading5: { result: function() { return exec(formatBlock, '<h5>'); }},
             heading6: { result: function() { return exec(formatBlock, '<h6>'); }},
             paragraph: { result: function() { return exec(formatBlock, '<p>'); }},
+            removeFormat: { result: function(){
+                let removedHTML = textVersion(formatBlock)
+                return exec(removedHTML, '<>');
+            }}
             quote: { result: function() { return exec(formatBlock, '<blockquote>'); }},
             orderedList: { state: function() { return queryCommandState('insertOrderedList'); }, result: function() { return exec('insertOrderedList'); }},
             unorderedList: { state: function() { return queryCommandState('insertUnorderedList'); },result: function() { return exec('insertUnorderedList'); }},
