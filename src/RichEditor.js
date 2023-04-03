b9d45e2fcf13645ce8f5aa1848a8f4567be90662
Gevorg Gasparyan <gevorg994@gmail.com>
2023-04-03 15:48:29 +0400

    feat: Add injectJavascript method
    (cherry picked from commit 5cdc67f14dc5838303e6470ade02e48eac7f3ca9)

--- src/RichEditor.js
+++ src/RichEditor.js
@@ -379,6 +379,10 @@ export default class RichTextEditor extends Component {
     }
   }
 
+  injectJavascript(script) {
+    return this.webviewBridge.injectJavaScript(script);
+  }
+
   preCode(type) {
     this.sendAction(actions.code, 'result', type);
   }
